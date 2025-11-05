8c32896cd501ea4e74c0906db62f9b787d9df2dc
Animesh Maity <maity.animesh157@gmail.com>
2025-11-05 17:03:27 +0530

    Updated routing

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { ThemeProvider } from "./context/ThemeContext";
-import { BrowserRouter } from "react-router-dom"; // ✅ import router
+import { HashRouter } from "react-router-dom"; // ✅ import router
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ThemeProvider>
         <App />
       </ThemeProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
