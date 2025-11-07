bff83dab92a544746363e8c44cbb3ecebc856780
Animesh Maity <maity.animesh157@gmail.com>
2025-11-07 20:44:01 +0530

    Aligned the all projects page project card type

--- src/components/sections/AllProjects.jsx
+++ src/components/sections/AllProjects.jsx
@@ -86,7 +86,7 @@ const AllProjects = () => {
         {filteredProjects.length > 0 ? (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             {filteredProjects.map((project) => (
-              <ProjectCard key={project.id} project={project} isFixed={false} />
+              <ProjectCard key={project.id} project={project} isFixed={true} />
             ))}
           </div>
         ) : (
